b42e763541e48e700c0cce2f82a3508e8000a2c7
bmbdlm <bmbdlm2001@gmail.com>
2020-12-25 23:12:39 +0800

    MERN-App

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Хийх зүйлсийн жагсаалтанд тавтай морил.</p>
+            </div>
+        )
+    }
+}
